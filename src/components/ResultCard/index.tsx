19f96c1222bb1ce243b954b5720bedf368297764
janaite <janaite@gmail.com>
2021-12-02 10:05:11 -0300

    API query using Axios

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -5,21 +5,36 @@ type ResultType = {
   followers: string;
   location: string;
   name: string;
-  avatar_url:string;
-}
+  avatar_url: string;
+  url: string;
+};
 
-const ResultCard = ( param : ResultType) => {
+const ResultCard = (param: ResultType) => {
   return (
     <div className="result-card-container">
       <div className="result-card-photo">
-          <img src={param.avatar_url} alt={param.name} />
+        <img src={param.avatar_url} alt={param.name} />
       </div>
       <div className="result-card-informations">
         <h1>Informações</h1>
-        <p><span className="result-card-label">Perfil:</span><span className="result-card-data">{param.login}</span></p>
-        <p><span className="result-card-label">Seguidores:</span><span className="result-card-data">{param.followers}</span></p>
-        <p><span className="result-card-label">Localidade:</span><span className="result-card-data">{param.location}</span></p>
-        <p><span className="result-card-label">Nome:</span><span className="result-card-data">{param.name}</span></p>
+        <p>
+          <span className="result-card-label">Perfil:</span>
+          <span className="result-card-data">
+            <a href={param.url}>{param.url}</a>
+          </span>
+        </p>
+        <p>
+          <span className="result-card-label">Seguidores:</span>
+          <span className="result-card-data">{param.followers}</span>
+        </p>
+        <p>
+          <span className="result-card-label">Localidade:</span>
+          <span className="result-card-data">{param.location}</span>
+        </p>
+        <p>
+          <span className="result-card-label">Nome:</span>
+          <span className="result-card-data">{param.name}</span>
+        </p>
       </div>
     </div>
   );
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,20 +1,77 @@
+import axios from "axios";
+import { useState } from "react";
 import ResultCard from "../../components/ResultCard";
 import "./styles.css";
 
+type FormData = {
+  username: string;
+};
+
+type ApiResultType = {
+  login: string;
+  id: number;
+  avatar_url: string;
+  url: string;
+  name: string;
+  company: string;
+  blog: string;
+  location: string;
+  followers: string;
+};
+
 const Search = () => {
+  const [githubResult, setGithubResult] = useState<ApiResultType>();
+
+  const [formData, setFormData] = useState<FormData>({
+    username: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${formData.username}`)
+      .then((response) => {
+        setGithubResult(response.data);
+      })
+      .catch((error) => {
+        setGithubResult(undefined);
+      });
+  };
+
   return (
     <div className="search-container">
       <div className="search-input-container">
         <h1>Encontre um perfil Github</h1>
-        <form>
-          <input type="text" placeholder="Usuário Github" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Usuário Github"
+            name="username"
+            onChange={handleChange}
+          />
           <button type="submit" className="btn btn-primary btn-lg start-button">
             Encontrar
           </button>
         </form>
       </div>
       <div className="search-result-container">
-        <ResultCard login="acnelio" followers="5400" name="Nelio Alves" location="Brazil" avatar_url="https://avatars.githubusercontent.com/u/13897257?v=4"/>
+        {githubResult && (
+          <ResultCard
+            login={githubResult.login}
+            url={githubResult.url}
+            followers={githubResult.followers}
+            name={githubResult.name}
+            location={githubResult.location}
+            avatar_url={githubResult.avatar_url}
+          />
+        )}
       </div>
     </div>
   );
