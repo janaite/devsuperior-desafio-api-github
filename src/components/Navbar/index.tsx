ec3905378778897131997fa95ba42bbb09425d26
janaite <janaite@gmail.com>
2021-12-02 10:14:42 -0300

    some final touches

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
-      <h1>GithubAPI</h1>
+      <Link to="/"><h1>GithubAPI</h1></Link>
     </div>
   );
 };
